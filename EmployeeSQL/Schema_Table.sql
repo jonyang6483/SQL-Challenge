dd0f7680f80a059babb7fc0ac54029efd2a22fd8
JonYang1992 <yangjonathan1992@gmail.com>
2024-05-27 07:05:21 -0400

    Update Schema_Table.sql

--- EmployeeSQL/Schema_Table.sql
+++ EmployeeSQL/Schema_Table.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
-	"emp_title" VARCHAR(20)   NOT NULL,
+    "emp_title" VARCHAR(20)   NOT NULL,
     "birth_date" DATE   NOT NULL,
     "first_name" VARCHAR(30)   NOT NULL,
     "last_name" VARCHAR(30)   NOT NULL,
